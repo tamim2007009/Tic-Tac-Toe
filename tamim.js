316dc8f8f134c2ae121fa47fffc7b89ef81ba1d1
Tamim <tamim2007009@stud.kuet.ac.bd>
2024-01-27 18:48:49 +0600

    game without draw case

---
+++ tamim.js
@@ -0,0 +1,78 @@
+
+let boxes=document.querySelectorAll(".box");
+let resetBtn=document.querySelector("#reset_btn");
+let newGameBtn=document.querySelector("#new_btn");
+let msgContainer=document.querySelector(".msg_container");
+let msg=document.querySelector("#msg");
+
+let turn0=true;
+const winPatterns=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [0,4,8],
+];
+
+const resetGame =()=>{
+          turn0=true;
+          enableBoxes();
+       msgContainer.classList.add("hide");
+}
+
+
+
+
+boxes.forEach((box)=>{
+   box.addEventListener("click",() =>{
+     console.log("box was clicked");
+     if(turn0){
+     box.innerHTML="0";
+    turn0=false;}
+    else{
+        box.innerHTML="X";
+        turn0=true;
+    }
+    box.disabled = true;
+    checkWinner();
+   });
+});
+
+const disableBoxes =()=>{
+  for(let box of boxes){
+    box.disabled=true;
+  }
+
+}
+const enableBoxes =()=>{
+    for(let box of boxes){
+      box.disabled=false;
+      box.innerHTML="";
+    }
+  
+  }
+const showWinner=(winner)=>{
+
+    msg.innerHTML = `Congratulations, Winner is ${winner}`;
+
+   msgContainer.classList.remove("hide");
+   disableBoxes();
+}
+const checkWinner = () => {
+      for(let pattern of winPatterns){
+     let pos1Val=boxes[pattern[0]].innerHTML;
+     let pos2Val=boxes[pattern[1]].innerHTML;
+     let pos3Val=boxes[pattern[2]].innerHTML;
+     if(pos1Val !="" && pos2Val!=""&&pos3Val !=""){
+        if(pos1Val==pos2Val && pos2Val==pos3Val){
+            console.log("Winner",pos1Val);
+            showWinner(pos1Val);
+        }
+     }
+      }
+};
+newGameBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
